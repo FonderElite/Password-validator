a0c2ec6be2cd8ecf70b7e14f002da0c936eba786
FonderElite <71190959+FonderElite@users.noreply.github.com>
2021-02-28 03:36:14 +0800

    Update validpass.cpp

--- validpass.cpp
+++ validpass.cpp
@@ -6,7 +6,10 @@ using namespace std;
 class Message {
 public:
 void msg(){
- cout<<"Anyways Thank You <3";
+ cout<<"<3 Anyways Thank You <3"<<endl;
+}
+void line(){
+    cout<<"==================================================";
 }
 };
 
@@ -27,17 +30,19 @@ bool is_sp_char(char chr) {
 
 int main() {
     Message mg;
-    char letters[] = "abcdefghijklmnopqrstuvwxyz)(@!^$#";
-    char x = letters[rand() % 32];
-    char a = letters[rand() % 31];
-    char b = letters[rand() % 30];
-    char c = letters[rand() % 31];
-    char d = letters[rand() % 32];
-    char e = letters[rand() % 31];
-    char f = letters[rand() % 30];
-    char g = letters[rand() % 31];
-    char h = letters[rand() % 30];
-    char i = letters[rand() % 30];
+    char letters[] = "abc@!^$#d@!^$#efghi@!^$#jklmnop@!^$#qrstuvwxyz)(@!^$#";
+    int randomnum = rand() % 2;
+    int randomnum_again = rand() % randomnum;
+    char x = letters[rand() % 30 + randomnum_again];
+    char a = letters[rand() % 30 + randomnum_again];
+    char b = letters[rand() % 30+ randomnum_again];
+    char c = letters[rand() % 30 + randomnum_again];
+    char d = letters[rand() % 30 + randomnum_again];
+    char e = letters[rand() % 30 + randomnum_again];
+    char f = letters[rand() % 30 + randomnum_again];
+    char g = letters[rand() % 30 + randomnum_again];
+    char h = letters[rand() % 30 + randomnum_again];
+    char i = letters[rand() % 30 + randomnum_again];
     bool has_number = false;
     bool has_sp_char = false;
     bool has_spaces = false;
@@ -45,7 +50,7 @@ int main() {
     cout<<"Password Validator"<<endl;
     cout << "Write your password: ";
     cin >> password;
-    if (password.length() >= 5 && password.length() <= 10) {
+    if (password.length() >= 5 && password.length() <= 32) {
         for (int i = 0; i < password.length(); i++) {
             if (is_number(password[i]) == true){
                 has_number = true;
@@ -60,13 +65,19 @@ int main() {
         if (has_number == true && has_sp_char == true && has_spaces == false) {
             cout << "Your password is valid!\n";
         }else{
-            cout << "Your password is not valid.(Try Adding Special Chars or Lengthen it)\n";
+             mg.line();
+            cout << "\nYour password is not valid.(Try Adding Special Chars or Lengthen it)\n";
+            cout << "Required Length:5(with special char and number)\n";
+            cout<<"Limit 32 chars\n";
              int random_numb1 = random_Pass(1234);
              int randomized = (rand() % random_numb1);
           cout << "Try something like: "<<x <<a <<b<<c<<d<<e<<f<<g<<h<<i<<randomized<<endl;
         }
     }else{
-        cout << "Password is too short!\n";
+        mg.line();
+        cout << "\nPassword is too short or No Special Chars included\n";
+          cout << "Required Length:5(with special char and number)\n";
+            cout<<"Limit: 32 chars\n";
         int random_numb2 = random_Pass(1234);
         int randomized2 = (rand() % random_numb2);
         cout << "Try something like: "<<x <<a <<b<<c<<d<<e<<f<<g<<h<<i<<randomized2<<endl;
